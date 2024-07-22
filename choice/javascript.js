d0cf053a21a39d4bb3d175b81cabd9786a4b1a08
sakthi <143791053+Sakthivishnum@users.noreply.github.com>
2024-07-22 16:45:45 +0530

    Add files via upload

---
+++ choice/javascript.js
@@ -0,0 +1,15 @@
+var message =document.getElementsByClassName("change");
+function red(){
+    document.getElementsByClassName("choice1")[0].style.backgroundColor ="red";
+}
+function red1(){
+    document.getElementsByClassName("choice2")[0].style.backgroundColor ="red";
+}function red2(){
+    document.getElementsByClassName("choice3")[0].style.backgroundColor ="red";
+}
+function green(){
+    document.getElementsByClassName("choice4")[0].style.backgroundColor ="green";
+}
+function green1(){
+    document.getElementsByClassName("choice5")[0].style.backgroundColor ="green"
+}
